0e39880678149cc4b7767485356a6ae563134129
Pianoopera <azing3469@gmail.com>
2022-03-13 13:10:53 +0900

    言語ファイル作成及び動作確認

---
+++ next-i18next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  /** i18n settings */
+  i18n: {
+    locales: ["ja"],
+    defaultLocale: "ja",
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,36 @@
 import type { NextPage } from "next";
 
-const Home: NextPage = () => {
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+/**
+ * @description
+ *  - Index.tsxファイルにはHeader,Footer,Routingを定義する
+ * @todo
+ *  - Header, Footer, Routing 追加
+ */
+const Home: NextPage = ({}) => {
+  const { t } = useTranslation('common')
+
   return (
     <>
-      <p className="text-red-400">next.js</p>
+      <p className="text-red-400">{t("header.siteName")}</p>
       <a
         href="https://github.com/quadraticvotingJP/quadratic-voting-jp"
         rel="noopener noreferrer"
         target="_blank"
         className="text-red-400"
       >
-        quadratic-voting-jp
+        {t("unique.screen.top.pageTitle.title")}
       </a>
     </>
   );
 };
+
+export const getStaticProps = async ({ locale = "ja" }) => ({
+  props: {
+    ...await serverSideTranslations(locale, ['common']),
+  },
+})
 
 export default Home;

4c1d14d96bf4a3a65af06182c55ab1ae38ef3f89
Pianoopera <azing3469@gmail.com>
2022-04-02 12:16:44 +0900

    #81 component Ecsystem

---
+++ components/ecosystems/EcVoteForm.tsx
@@ -0,0 +1,11 @@
+/** @description Ecosystem Vote Form */
+import React from "react";
+import { useTranslation } from "next-i18next";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { utilsValidationRule } from "@/utils/validation";
+
+const EcVoteForm: React.FC = () => {
+  return <></>;
+};
+
+export default EcVoteForm;
--- components/ecosystems/EntryPoint.tsx
+++ components/ecosystems/EntryPoint.tsx
@@ -1,5 +1,6 @@
 // 上位componentで毎回importするのを省くためまとめる
 export { default as EcAdvertisement } from "@/components/ecosystems/EcAdvertisement";
 export { default as EcExplanation } from "@/components/ecosystems/EcExplanation";
+export { default as EcVoteForm } from "@/components/ecosystems/EcVoteForm";
 export { default as EcCreateForm } from "@/components/ecosystems/EcCreateForm";
 export { default as EcDashboard } from "@/components/ecosystems/EcDashboard";

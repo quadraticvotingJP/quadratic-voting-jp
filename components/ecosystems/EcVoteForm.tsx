bb8d0893a8e07525a5cd1c4d703f153d5121e10d
Pianoopera <azing3469@gmail.com>
2022-04-09 18:31:19 +0900

    #81 vote page mock 作成

--- components/ecosystems/EcVoteForm.tsx
+++ components/ecosystems/EcVoteForm.tsx
@@ -1,11 +1,61 @@
 /** @description Ecosystem Vote Form */
 import React from "react";
+import { GetStaticPaths } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { utilsValidationRule } from "@/utils/validation";
+// component
+import { MoButtons } from "@/components/molecules/EntryPoint";
+import { OrCardText } from "@/components/organisms/EntryPoint";
 
 const EcVoteForm: React.FC = () => {
-  return <></>;
+  const { t } = useTranslation("common");
+  return (
+    <>
+      <OrCardText
+        title={t("common.event.eventTitle.title")}
+        required={false}
+        contents={"次の都知事は誰？"}
+        showEdit={false}
+        disabled={false}
+      />
+      <br />
+      <OrCardText
+        title={t("common.event.overview.title")}
+        required={false}
+        contents={"都知事を決めるための選挙を行います"}
+        showEdit={false}
+        disabled={false}
+      />
+      <br />
+      <MoButtons
+        left={{ title: "-", disabled: false, onClick: () => {} }}
+        right={{ title: "+", disabled: false, onClick: () => {} }}
+      />
+    </>
+  );
 };
 
 export default EcVoteForm;
+// // i18n
+// export const getStaticProps = async ({ locale = "ja" }) => ({
+//   props: {
+//     ...(await serverSideTranslations(locale, ["common"])),
+//   },
+// });
+
+// // getStaticPaths:動的なルーティングを（ダイナミックルーティング）Next.jsで設定する際に使用
+// // ビルド時に実行
+// // https://y-hiroyuki.xyz/next-js/getstaticpaths
+// export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
+//   return {
+//     // pathsは、どのパスをPre-renderingsするか指定
+//     paths: [], //ビルド時にページを作成する必要がないことを示す
+
+//     // fallbackは、指定されたパスがtrueかfalseかで返す値を決定するもの
+//     // true:返されるのは事前に生成されたHTML
+//     // false: 生成されていないパスは全て「404」
+//     fallback: "blocking", //フォールバックの種類
+//   };
+// };
--- pages/vote/[id].tsx
+++ pages/vote/[id].tsx
@@ -1,53 +1,65 @@
-import { GetStaticPaths } from "next";
+// i18n
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { useTranslation } from "next-i18next";
 // component
-import { MoButtons } from "@/components/molecules/EntryPoint";
-import { OrCardText } from "@/components/organisms/EntryPoint";
-const Id = ({}) => {
-  const { t } = useTranslation("common");
-  return (
-    <>
-      <OrCardText
-        title={t("common.event.eventTitle.title")}
-        required={false}
-        contents={"次の都知事は誰？"}
-        showEdit={false}
-        disabled={false}
-      />
-      <br />
-      <OrCardText
-        title={t("common.event.overview.title")}
-        required={false}
-        contents={"都知事を決めるための選挙を行います"}
-        showEdit={false}
-        disabled={false}
-      />
-      <br />
-      <MoButtons
-        left={{ title: "-", disabled: false, onClick: () => {} }}
-        right={{ title: "+", disabled: false, onClick: () => {} }}
-      />
-    </>
-  );
-};
+import { EcVoteForm } from "@/components/ecosystems/EntryPoint";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { ParsedUrlQuery } from "querystring";
 
+const Id = ({}) => {
+  return <EcVoteForm />;
+};
 export default Id;
 
+export interface Item {
+  id: string;
+}
+
+interface Props {
+  item: Item;
+}
+
+// 1. Paramsの型を定義し、ParsedUrlQueryをextendsする
+interface Params extends ParsedUrlQuery {
+  id: string;
+}
+
 // i18n
-export const getStaticProps = async ({ locale = "ja" }) => ({
-  props: {
-    ...(await serverSideTranslations(locale, ["common"])),
-  },
-});
+export const getStaticProps: GetStaticProps<Props, Params> = async ({
+  locale = "ja",
+  params,
+}) => {
+  const itemf = params?.id;
+  const item = {
+    id: "a",
+  };
+  return {
+    props: {
+      item,
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
+};
 
 // getStaticPaths:動的なルーティングを（ダイナミックルーティング）Next.jsで設定する際に使用
 // ビルド時に実行
 // https://y-hiroyuki.xyz/next-js/getstaticpaths
-export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
+export const getStaticPaths: GetStaticPaths<{ id: string }> = async () => {
+  const items = [
+    {
+      id: "a",
+    },
+    {
+      id: "b",
+    },
+  ];
+  const paths = items.map(({ id }) => ({
+    params: {
+      id: id,
+    },
+  }));
   return {
     // pathsは、どのパスをPre-renderingsするか指定
-    paths: [], //ビルド時にページを作成する必要がないことを示す
+    paths, //ビルド時にページを作成する必要がないことを示す
 
     // fallbackは、指定されたパスがtrueかfalseかで返す値を決定するもの
     // true:返されるのは事前に生成されたHTML

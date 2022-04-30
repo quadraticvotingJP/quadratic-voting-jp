b9a6c2c7877645429663b373fd5ce80ebe420e15
KOHEI183 <zbtbfuufp@gmail.com>
2022-04-30 19:09:59 +0900

    疎通commitからQiitaの記事でコンパイルDone #129

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,12 @@
 /* eslint-disable indent */
 import * as functions from "firebase-functions";
-
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-
-export const helloWorld = functions.https.onRequest(
-  // eslint-disable-next-line indent
-  (request: any, response: any) => {
-    // eslint-disable-next-line indent
-    // eslint-disable-next-line object-curly-spacing
-    functions.logger.info("Hello logs!", { structuredData: true });
-    // eslint-disable-next-line indent
-    response.send("Hello from Firebase!");
-    // eslint-disable-next-line indent
-  }
-);
+import next from "next";
+const dev = false;
+// eslint-disable-next-line object-curly-spacing
+const app = next({ dev, conf: { distDir: "next" } });
+const handle = app.getRequestHandler();
+// eslint-disable-next-line indent
+export const nextApp = functions.https.onRequest((req, res) => {
+  console.log("File: " + req.originalUrl);
+  return app.prepare().then(() => handle(req, res));
+});

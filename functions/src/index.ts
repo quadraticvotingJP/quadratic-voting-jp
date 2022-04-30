9eca3bd9b16b86b3643c6f77263f92adc0652daa
KOHEI183 <zbtbfuufp@gmail.com>
2022-04-30 17:48:14 +0900

    function疎通ケース #129

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,17 @@
+/* eslint-disable indent */
 import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = functions.https.onRequest(
+  // eslint-disable-next-line indent
+  (request: any, response: any) => {
+    // eslint-disable-next-line indent
+    // eslint-disable-next-line object-curly-spacing
+    functions.logger.info("Hello logs!", { structuredData: true });
+    // eslint-disable-next-line indent
+    response.send("Hello from Firebase!");
+    // eslint-disable-next-line indent
+  }
+);

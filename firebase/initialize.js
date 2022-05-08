ba9ffe6e77187a1aae2c99e179a5e109b800e7c0
KOHEI183 <zbtbfuufp@gmail.com>
2022-05-08 13:53:20 +0900

    initializeの変更 #167

--- firebase/initialize.js
+++ firebase/initialize.js
@@ -1,16 +1,15 @@
 import { initializeApp, apps, app } from "firebase/compat/app";
 import { getAuth } from "firebase/auth";
 
-if (!apps.length) {
-  const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
-  };
-}
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
+};
+
 // initialize
 export const firebase = !apps ? initializeApp(firebaseConfig) : app();
 // authentication

0ae96b7e61e97bd5090b61350ffc98f45622660e
KOHEI183 <zbtbfuufp@gmail.com>
2022-05-19 10:47:51 +0900

    buttoncomponentのMUIを未使用にする #227

--- components/organisms/OrCardBar.tsx
+++ components/organisms/OrCardBar.tsx
@@ -47,6 +47,7 @@ const OrCardBar: React.FC<Props> = ({
         </div>
         <div className="flex justify-center">
           <AtButton
+            className="bg-black-900 hover:bg-black-900 hover:bg-opacity-80 text-white text-xs w-40 h-10 py-2 px-6 rounded"
             title={button.title}
             disabled={button.disabled}
             size={button.size}

ff7de2019dbbbbafbdd8843b48979d16af77bac6
Pianoopera <azing3469@gmail.com>
2022-05-28 11:23:16 +0900

    投票コンポーネント修正

--- components/organisms/OrProposalBlocks.tsx
+++ components/organisms/OrProposalBlocks.tsx
@@ -4,19 +4,21 @@ import React, { useEffect, useRef } from "react";
 interface Props {
   cost: number;
   color: string;
+  denominator: number;
 }
 
 // eslint-disable-next-line react/display-name
 const OrProposalBlocks: React.FC<Props> = React.memo(
-  ({ cost, color = "bg-blue-900" }) => {
+  ({ cost, color = "bg-blue-900", denominator }) => {
     useEffect(() => {}, [cost]);
 
     return (
       <>
         <Card
-          className={`${color} top-64 py-12 px-12 right-44 text-white fixed text-center`}
+          className={`${color} top-64 py-6 px-6 right-8 text-white fixed text-center`}
         >
-          <p className="m-0">{cost} Credits</p>
+          <p className="m-0">{cost + `/` + denominator}</p>
+          <p className="text-xs">Credits</p>
         </Card>
       </>
     );

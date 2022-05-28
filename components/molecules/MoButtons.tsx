59a424845cb9403e44081d0ace00486815451738
Pianoopera <azing3469@gmail.com>
2022-05-28 11:46:04 +0900

    投票ボタンスタイル反映

--- components/molecules/MoButtons.tsx
+++ components/molecules/MoButtons.tsx
@@ -18,16 +18,16 @@ type Props = {
 const MoButtons: React.FC<Props> = ({ left, right }) => {
   const { t } = useTranslation("common");
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-center">
       <AtButton
-        className="bg-red-900	hover:bg-red-900 text-white w-full mr-5 text-base w-40 h-10 py-2 px-6 rounded"
+        className="hover:border-blue-200 hover:text-blue-200 w-full mr-5 text-base w-40 h-10 py-2 px-6 rounded-full border-2 border-blue-900 text-blue"
         title={left.title}
         onClick={left.onClick}
         disabled={left.disabled}
       />
       {/* 2つのButtonのサイズ感がずれるためクラスで流す */}
       <AtButton
-        className="bg-blue-900 hover:bg-blue-900	text-white w-full text-base w-40 h-10 py-2 px-6 rounded"
+        className="bg-blue-900 hover:bg-blue-300	text-white w-full text-base w-40 h-10 py-2 px-6 rounded-full"
         title={right.title}
         onClick={right.onClick}
         disabled={right.disabled}

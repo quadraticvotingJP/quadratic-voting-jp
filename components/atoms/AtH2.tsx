53731b56ed759497f1877423926aa7ff14c7f204
Pianoopera <azing3469@gmail.com>
2022-05-28 18:21:29 +0900

    タイトル修正

--- components/atoms/AtH2.tsx
+++ components/atoms/AtH2.tsx
@@ -6,6 +6,6 @@ type Props = {
 
 // eslint-disable-next-line react/display-name
 const AtH2: React.FC<Props> = React.memo(({ title }) => {
-  return <h2 className="font-bold">{title}</h2>;
+  return <h2 className="text-center mb-16 text-4xl font-bold">{title}</h2>;
 });
 export default AtH2;

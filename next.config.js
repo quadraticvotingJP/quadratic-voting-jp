2438b8c6b8cf57a983cebc8a297dd5c67fe31b31
KOHEI183 <zbtbfuufp@gmail.com>
2022-05-31 20:32:59 +0900

    confからenvの削除 #83

--- next.config.js
+++ next.config.js
@@ -5,9 +5,6 @@ const { i18n } = require("./next-i18next.config");
 const nextConfig = {
   reactStrictMode: true,
   i18n,
-  env: {
-    GA_TRACKING_ID: process.env.NEXT_PUBLIC_GA_TRACKING_ID,
-  },
 };
 
 module.exports = nextConfig;

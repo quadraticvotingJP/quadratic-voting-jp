b472038674e81d8be9e894641c66084d6808b167
KOHEI183 <zbtbfuufp@gmail.com>
2022-06-05 15:59:37 +0900

    min-widthの設定 #165

--- components/ecosystems/EcLp.tsx
+++ components/ecosystems/EcLp.tsx
@@ -31,9 +31,9 @@ const EcLp: React.FC<Props> = ({ images }) => {
         </JustifyCenterElement>
       </FirstView>
 
-      <SectionIsQuadraticVoting id="isQuadraticVoting">
-        <IsQuadraticVoting>
-          <div>
+      <Section id="isQuadraticVoting">
+        <JustifyBetweenMaxWidth1000>
+          <IsQuadraticVotingOverView>
             <IsQuadraticVotingTitle>
               {t("lp.isQuadraticVoting.title")}
             </IsQuadraticVotingTitle>
@@ -49,8 +49,8 @@ const EcLp: React.FC<Props> = ({ images }) => {
               disabled={false}
               onClick={moveCreateEvent}
             />
-          </div>
-          <div>
+          </IsQuadraticVotingOverView>
+          <IsQuadraticVotingImageArea>
             <IsQuadraticVotingImageElement>
               <Text>{t("lp.isQuadraticVoting.conventional")}</Text>
               <AtImage
@@ -71,145 +71,156 @@ const EcLp: React.FC<Props> = ({ images }) => {
                 height={174}
               />
             </IsQuadraticVotingImageElement>
-          </div>
-        </IsQuadraticVoting>
-      </SectionIsQuadraticVoting>
+          </IsQuadraticVotingImageArea>
+        </JustifyBetweenMaxWidth1000>
+      </Section>
+
       <Section id="feature">
-        <FeatureTitle>{t("lp.feature.title")}</FeatureTitle>
-        <DirectionColElement>
-          <FeatureElement>
-            <AtImage
-              src={images[2].toString()}
-              alt="no1Feature"
-              layout="intrinsic"
-              width={360}
-              height={240}
-            />
-            <FeatureOverview>
-              <FeatureNo>{t("lp.feature.no1")}</FeatureNo>
-              <Feature>{t("lp.feature.no1Feature")}</Feature>
-              <Text>{t("lp.feature.no1Explanation")}</Text>
-            </FeatureOverview>
-          </FeatureElement>
-          <FeatureElement>
-            <FeatureOverview>
-              <FeatureNo>{t("lp.feature.no2")}</FeatureNo>
-              <Feature>{t("lp.feature.no2Feature")}</Feature>
-              <Text>{t("lp.feature.no2Explanation")}</Text>
-            </FeatureOverview>
-            <AtImage
-              src={images[2].toString()}
-              alt="no2Feature"
-              layout="intrinsic"
-              width={360}
-              height={240}
-            />
-          </FeatureElement>
-          <FeatureElement>
-            <AtImage
-              src={images[2].toString()}
-              alt="no3Feature"
-              layout="intrinsic"
-              width={360}
-              height={240}
-            />
-            <FeatureOverview>
-              <FeatureNo>{t("lp.feature.no3")}</FeatureNo>
-              <Feature>{t("lp.feature.no3Feature")}</Feature>
-              <Text>{t("lp.feature.no3Explanation")}</Text>
-            </FeatureOverview>
-          </FeatureElement>
-        </DirectionColElement>
+        <DirectionColMaxWidth1000>
+          <FeatureTitle>{t("lp.feature.title")}</FeatureTitle>
+          <FeatureArea>
+            <FeatureElement>
+              <AtImage
+                src={images[2].toString()}
+                alt="no1Feature"
+                layout="intrinsic"
+                width={400}
+                height={240}
+              />
+              <FeatureOverview>
+                <FeatureNo>{t("lp.feature.no1")}</FeatureNo>
+                <Feature>{t("lp.feature.no1Feature")}</Feature>
+                <Text>{t("lp.feature.no1Explanation")}</Text>
+              </FeatureOverview>
+            </FeatureElement>
+            <FeatureElement>
+              <FeatureOverview>
+                <FeatureNo>{t("lp.feature.no2")}</FeatureNo>
+                <Feature>{t("lp.feature.no2Feature")}</Feature>
+                <Text>{t("lp.feature.no2Explanation")}</Text>
+              </FeatureOverview>
+              <AtImage
+                src={images[2].toString()}
+                alt="no2Feature"
+                layout="intrinsic"
+                width={400}
+                height={240}
+              />
+            </FeatureElement>
+            <FeatureElement>
+              <AtImage
+                src={images[2].toString()}
+                alt="no3Feature"
+                layout="intrinsic"
+                width={400}
+                height={240}
+              />
+              <FeatureOverview>
+                <FeatureNo>{t("lp.feature.no3")}</FeatureNo>
+                <Feature>{t("lp.feature.no3Feature")}</Feature>
+                <Text>{t("lp.feature.no3Explanation")}</Text>
+              </FeatureOverview>
+            </FeatureElement>
+          </FeatureArea>
+        </DirectionColMaxWidth1000>
       </Section>
 
       <Section id="rule">
-        <RuleTitle>{t("lp.rule.title")}</RuleTitle>
-        <DirectionColElement>
-          <RuleElement>
-            <div>
+        <DirectionColMaxWidth1000>
+          <RuleTitle>{t("lp.rule.title")}</RuleTitle>
+          <DirectionColElement>
+            <RuleElement>
+              <div>
+                <AtImage
+                  src={images[3].toString()}
+                  alt="rule1"
+                  layout="intrinsic"
+                  width={229}
+                  height={194}
+                />
+                <RuleText>{t("lp.rule.rule1")}</RuleText>
+              </div>
+              <div>
+                <AtImage
+                  src={images[3].toString()}
+                  alt="rule2"
+                  layout="intrinsic"
+                  width={229}
+                  height={194}
+                />
+                <RuleText>{t("lp.rule.rule2")}</RuleText>
+              </div>
+              <div>
+                <AtImage
+                  src={images[3].toString()}
+                  alt="rule3"
+                  layout="intrinsic"
+                  width={229}
+                  height={194}
+                />
+                <RuleText>{t("lp.rule.rule3")}</RuleText>
+              </div>
+            </RuleElement>
+            <RuleExampleElement>
+              <div>
+                <RuleExample1>{t("lp.rule.example1")}</RuleExample1>
+                <RuleExample1>{t("lp.rule.example2")}</RuleExample1>
+                <RuleExample2>{t("lp.rule.example3")}</RuleExample2>
+                <AtHref
+                  blank={true}
+                  title={t("lp.rule.urlTitle")}
+                  link={t("lp.rule.url")}
+                  className="text-blue-500 text-sm"
+                />
+                <RuleExample3></RuleExample3>
+                <AtButton
+                  className="bg-black-900 hover:bg-black-900 hover:bg-opacity-80 text-white text-base w-40 h-12 py-2 px-6 w-64 rounded disabled:bg-slate-300"
+                  title={t("common.button.startNow")}
+                  disabled={false}
+                  onClick={moveCreateEvent}
+                />
+              </div>
               <AtImage
-                src={images[3].toString()}
-                alt="rule1"
+                src={images[4].toString()}
+                alt="point4"
                 layout="intrinsic"
-                width={229}
-                height={194}
+                width={400}
+                height={400}
               />
-              <RuleText>{t("lp.rule.rule1")}</RuleText>
-            </div>
-            <div>
-              <AtImage
-                src={images[3].toString()}
-                alt="rule2"
-                layout="intrinsic"
-                width={229}
-                height={194}
-              />
-              <RuleText>{t("lp.rule.rule2")}</RuleText>
-            </div>
-            <div>
-              <AtImage
-                src={images[3].toString()}
-                alt="rule3"
-                layout="intrinsic"
-                width={229}
-                height={194}
-              />
-              <RuleText>{t("lp.rule.rule3")}</RuleText>
-            </div>
-          </RuleElement>
-          <RuleExampleElement>
-            <div>
-              <RuleExample1>{t("lp.rule.example1")}</RuleExample1>
-              <RuleExample1>{t("lp.rule.example2")}</RuleExample1>
-              <RuleExample2>{t("lp.rule.example3")}</RuleExample2>
-              <AtHref
-                blank={true}
-                title={t("lp.rule.urlTitle")}
-                link={t("lp.rule.url")}
-                className="text-blue-500 text-sm"
-              />
-              <RuleExample3></RuleExample3>
-              <AtButton
-                className="bg-black-900 hover:bg-black-900 hover:bg-opacity-80 text-white text-base w-40 h-12 py-2 px-6 w-64 rounded disabled:bg-slate-300"
-                title={t("common.button.startNow")}
-                disabled={false}
-                onClick={moveCreateEvent}
-              />
-            </div>
-            <AtImage
-              src={images[4].toString()}
-              alt="point4"
-              layout="intrinsic"
-              width={400}
-              height={400}
-            />
-          </RuleExampleElement>
-        </DirectionColElement>
+            </RuleExampleElement>
+          </DirectionColElement>
+        </DirectionColMaxWidth1000>
       </Section>
+
       <Section id="scene">
-        <SceneTitle>{t("lp.scene.title")}</SceneTitle>
-        <DirectionColElement>
-          <SceneListTop>
-            <Text>{t("lp.scene.scene1")}</Text>
-            <Text>{t("lp.scene.scene2")}</Text>
-            <Text>{t("lp.scene.scene3")}</Text>
-          </SceneListTop>
-          <SceneListBottom>
-            <Text>{t("lp.scene.scene4")}</Text>
-            <Text>{t("lp.scene.scene5")}</Text>
-          </SceneListBottom>
-        </DirectionColElement>
+        <DirectionColMaxWidth1000>
+          <SceneTitle>{t("lp.scene.title")}</SceneTitle>
+          <DirectionColElement>
+            <SceneListTop>
+              <Text>{t("lp.scene.scene1")}</Text>
+              <Text>{t("lp.scene.scene2")}</Text>
+              <Text>{t("lp.scene.scene3")}</Text>
+            </SceneListTop>
+            <SceneListBottom>
+              <Text>{t("lp.scene.scene4")}</Text>
+              <Text>{t("lp.scene.scene5")}</Text>
+            </SceneListBottom>
+          </DirectionColElement>
+        </DirectionColMaxWidth1000>
       </Section>
+
       <Section id="startNow">
-        <StartNowTitle>{t("lp.startNow.title")}</StartNowTitle>
-        <JustifyCenterElement>
-          <AtButton
-            className="bg-black-900 hover:bg-black-900 hover:bg-opacity-80 text-white text-base w-40 h-12 py-2 px-6 w-64 rounded disabled:bg-slate-300"
-            title={t("common.button.startNow")}
-            disabled={false}
-            onClick={moveCreateEvent}
-          />
-        </JustifyCenterElement>
+        <MaxWidth1000>
+          <StartNowTitle>{t("lp.startNow.title")}</StartNowTitle>
+          <JustifyCenterElement>
+            <AtButton
+              className="bg-black-900 hover:bg-black-900 hover:bg-opacity-80 text-white text-base w-40 h-12 py-2 px-6 w-64 rounded disabled:bg-slate-300"
+              title={t("common.button.startNow")}
+              disabled={false}
+              onClick={moveCreateEvent}
+            />
+          </JustifyCenterElement>
+        </MaxWidth1000>
       </Section>
     </>
   );
@@ -218,9 +229,10 @@ export default EcLp;
 
 // Base css
 const Section = styled.section`
+  display: flex;
+  justify-content: center;
   margin-bottom: 160px;
 `;
-
 const BaseTitle = styled.p`
   font-size: 20px;
   white-space: pre-wrap;
@@ -247,9 +259,18 @@ const DirectionColElement = styled(FlexElement)`
   flex-direction: column;
   align-items: center;
 `;
+const MaxWidth1000 = styled.div`
+  max-width: 1000px;
+`;
+const JustifyBetweenMaxWidth1000 = styled(JustifyBetweenElement)`
+  max-width: 1000px;
+`;
+const DirectionColMaxWidth1000 = styled(DirectionColElement)`
+  max-width: 1000px;
+`;
 
 // first view
-const FirstView = styled.div<{ image: string }>`
+const FirstView = styled.section<{ image: string }>`
   padding: 185px 100px 158px 100px;
   margin-bottom: 118px;
   background-image: url(${(props) => props.image});
@@ -268,12 +289,11 @@ const FirstViewSubTitle = styled.p`
 `;
 
 // isQuadraticVoting
-const SectionIsQuadraticVoting = styled(Section)`
-  display: flex;
-  justify-content: center;
+const IsQuadraticVotingOverView = styled.div`
+  width: 65%;
 `;
-const IsQuadraticVoting = styled(JustifyBetweenElement)`
-  width: 70%;
+const IsQuadraticVotingImageArea = styled.div`
+  width: 30%;
 `;
 const IsQuadraticVotingTitle = styled(BaseTitle)`
   margin-bottom: 32px;
@@ -293,12 +313,15 @@ const FeatureTitle = styled(BaseTitle)`
   margin-bottom: 80px;
   text-align: center;
 `;
+const FeatureArea = styled(DirectionColElement)`
+  width: 877px;
+`;
 const FeatureElement = styled(JustifyBetweenElement)`
-  width: 65%;
+  width: 100%;
   margin-bottom: 48px;
 `;
 const FeatureOverview = styled.div`
-  width: 390px;
+  width: 400px;
 `;
 const FeatureNo = styled.p`
   font-size: 40px;
@@ -317,14 +340,14 @@ const RuleTitle = styled(BaseTitle)`
   text-align: center;
 `;
 const RuleElement = styled(JustifyBetweenElement)`
-  width: 70%;
+  width: 100%;
   margin-bottom: 80px;
 `;
 const RuleText = styled(Text)`
   text-align: center;
 `;
 const RuleExampleElement = styled(JustifyBetweenElement)`
-  width: 70%;
+  width: 100%;
 `;
 const RuleExample1 = styled(Text)`
   margin-bottom: 24px;
@@ -342,12 +365,12 @@ const SceneTitle = styled(BaseTitle)`
   text-align: center;
 `;
 const SceneListTop = styled(JustifyBetweenElement)`
-  width: 70%;
+  width: 804px;
   margin-bottom: 51px;
   text-align: center;
 `;
 const SceneListBottom = styled(JustifyBetweenElement)`
-  width: 50%;
+  width: 475px;
   margin-bottom: 163px;
   text-align: center;
 `;

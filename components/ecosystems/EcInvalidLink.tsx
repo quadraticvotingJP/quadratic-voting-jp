9459fe5f839e31d8051939c2c8c95178d5b51a24
KOHEI183 <zbtbfuufp@gmail.com>
2022-06-10 18:44:39 +0900

    リンクの生成 #288

--- components/ecosystems/EcInvalidLink.tsx
+++ components/ecosystems/EcInvalidLink.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { useTranslation } from "next-i18next";
+// component
+import { AtHref } from "@/components/atoms/EntryPoint";
 
 const EcInvalidLink: React.FC = () => {
   const { t } = useTranslation("common");
 
   return (
     <>
-      <div className="flex justify-center content-center">
+      <div className="flex justify-center content-center mb-6">
         <p>{t("common.systemError.invalidUrl")}</p>
+      </div>
+      <div className="flex justify-center content-center">
+        <AtHref
+          title={t("common.systemError.indexPushComment")}
+          link={t("common.systemError.indexPushURL")}
+          className="text-blue-500"
+        />
       </div>
     </>
   );

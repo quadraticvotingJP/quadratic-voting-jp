6af636f805e84a41c2ea275be2e8f9e3267d82bc
KOHEI183 <zbtbfuufp@gmail.com>
2022-06-10 17:11:55 +0900

    loading画面の実装 #268

--- components/ecosystems/EcLoading.tsx
+++ components/ecosystems/EcLoading.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import styled from "styled-components";
+import ReactLoading from "react-loading";
 
+// https://maasaablog.com/development/react/1149/
 // eslint-disable-next-line react/display-name
 const EcLoading: React.FC = () => {
-  return <div>loading</div>;
+  return (
+    <Loading>
+      <div>
+        <ReactLoading
+          type="spin"
+          color="#2f9bff"
+          width={100}
+          height={100}
+          className="mx-auto"
+        />
+        <Text>Loading...</Text>
+      </div>
+    </Loading>
+  );
 };
 export default EcLoading;
+
+const Loading = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Text = styled.p`
+  margin-top: 10px;
+  text-align: center;
+`;

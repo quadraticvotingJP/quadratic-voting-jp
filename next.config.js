5eecf714e49a499a78ab0baa72000e85b39ee2c5
KOHEI183 <zbtbfuufp@gmail.com>
2022-06-15 20:02:29 +0900

    広告の再配置 #338

--- next.config.js
+++ next.config.js
@@ -10,14 +10,18 @@ const nextConfig = {
       // firebase storage
       "firebasestorage.googleapis.com",
       //a8
-      "www27.a8.net",
-      "www10.a8.net",
       "px.a8.net",
-      "www15.a8.net",
+      "www27.a8.net",
+      "www26.a8.net",
+      "www22.a8.net",
       "www21.a8.net",
       "www16.a8.net",
-      "www26.a8.net",
+      "www15.a8.net",
       "www13.a8.net",
+      "www12.a8.net",
+      "www11.a8.net",
+      "www10.a8.net",
+      "www8.a8.net",
     ],
   },
 };

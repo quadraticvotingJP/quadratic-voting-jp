d095c088dd4ae395f834150a5b59efd25abc4993
KOHEI183 <zbtbfuufp@gmail.com>
2022-06-19 15:08:13 +0900

    AtImageのディレクトリを変更 #343

--- components/atoms/EntryPoint.tsx
+++ components/atoms/EntryPoint.tsx
@@ -1,7 +1,7 @@
 // 上位componentで毎回importするのを省くためまとめる
 export { AtBar } from "@/components/atoms/AtBar/index";
 export { default as AtButton } from "@/components/atoms/AtButton";
-export { default as AtImage } from "@/components/atoms/AtImage";
+export { AtImage } from "@/components/atoms/AtImage/index";
 export { default as AtH2 } from "@/components/atoms/AtH2";
 export { default as AtHref } from "@/components/atoms/AtHref";
 export { default as AtIconButton } from "@/components/atoms/AtIconButton";

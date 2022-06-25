849331a0581f2b2965c716b40abe7d4c322849b0
Pianoopera <azing3469@gmail.com>
2022-06-25 10:29:22 +0900

    投票完了後の遷移修正

--- pages/vote/[id].tsx
+++ pages/vote/[id].tsx
@@ -81,6 +81,25 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   // 投票用のKeyを取得した選択肢毎に追加する
   const conversionVoteData = conversion(event);
 
+  const { beforePublicationStartDate, afterPublicationEndDate } =
+    eventDateAuthorize(); // 日にちチェック
+  const dateBefore: boolean = beforePublicationStartDate(
+    conversionVoteData.publicationStartDate
+  ); // 開始前か確認
+  const dateAfter: boolean = afterPublicationEndDate(
+    conversionVoteData.publicationEndDate
+  ); // 終了後か確認
+
+  // 開始日と終了日内か確認
+  if (dateBefore || dateAfter) {
+    return {
+      props: {
+        documentId,
+        ...(await serverSideTranslations(context.locale!, ["common"])),
+      },
+    };
+  }
+
   // イベントが存在し、未回答の場合のリターン
   return {
     props: {
@@ -100,20 +119,7 @@ const Id = ({
   isAnswer = true,
   cantVote = false,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  const { beforePublicationStartDate, afterPublicationEndDate } =
-    eventDateAuthorize(); // 日にちチェック
-  const dateBefore: boolean = beforePublicationStartDate(
-    conversionVoteData.publicationStartDate
-  ); // 開始前か確認
-  const dateAfter: boolean = afterPublicationEndDate(
-    conversionVoteData.publicationEndDate
-  ); // 終了後か確認
-
   if (!documentId === null || isAnswer || cantVote || query === null) {
-    return <EcInvalidLink />;
-  }
-  // 開始日と終了日内か確認
-  if (dateBefore || dateAfter) {
     return <EcInvalidLink />;
   }
 

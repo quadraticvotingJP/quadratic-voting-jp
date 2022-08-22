24e888e12af8dd1a38268e768cad080708efe7ff
KOHEI183 <zbtbfuufp@gmail.com>
2022-08-22 17:06:41 +0900

    Clarityの導入 #550

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -81,6 +81,22 @@ export default class MyDocument extends Document {
                   console.error("Script failed to load:", e);
                 }}
               />
+              {/* google clarity */}
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    (function(c,l,a,r,i,t,y){
+                      c[a] = c[a] || function () { (c[a].q = c[a].q ||
+                      []).push(arguments) };
+                      t=l.createElement(r);
+                      t.async=1;
+                      t.src="https://www.clarity.ms/tag/"+i;
+                      y=l.getElementsByTagName(r)[0];
+                      y.parentNode.insertBefore(t,y);
+                    })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY}");
+                  `,
+                }}
+              />
             </>
           )}
         </Head>
